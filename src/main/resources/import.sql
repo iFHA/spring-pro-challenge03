e4d1080350c759f01b622d3337d4f577be895cf3
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-11 20:38:23 -0300

    seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+insert into tb_client (name, cpf, income, birth_date, children) values ("Yasmin Sandra Brenda Cardoso", "39309003006", 1200.2, "1969-02-05", 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Mateus Theo Mendes", "34195423562", 3500, "1989-01-13", 3);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Alícia Ana Elisa Barbosa", "79078101679", 2200, "1996-02-18", 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Alana Jennifer Isabela da Luz", "71145748600", 1800, "1985-01-12", 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Yago Bruno Rezende", "03728140856", 1500, "1987-09-18", 0);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Jorge Breno Luan Ramos", "15366956889", 3700, "1978-10-28", 4);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Noah Geraldo Emanuel Farias", "45834869289", 4500, "1999-12-26", 2);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Emanuelly Daiane Oliveira", "06668421830", 1300, "1998-03-19", 0);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Henrique Juan Samuel Viana", "87101622240", 1120, "1997-01-22", 1);
+insert into tb_client (name, cpf, income, birth_date, children) values ("Otávio Ian Oliveira", "71111226075", 1220, "1995-04-17", 1);
