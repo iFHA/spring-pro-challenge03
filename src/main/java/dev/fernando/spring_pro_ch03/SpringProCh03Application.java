e54e436a5dfd3e982caad297068080683d5ba167
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-11 20:16:25 -0300

    primeiro commit

---
+++ src/main/java/dev/fernando/spring_pro_ch03/SpringProCh03Application.java
@@ -0,0 +1,13 @@
+package dev.fernando.spring_pro_ch03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProCh03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProCh03Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fernando/spring_pro_ch03/SpringProCh03ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernando.spring_pro_ch03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProCh03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
