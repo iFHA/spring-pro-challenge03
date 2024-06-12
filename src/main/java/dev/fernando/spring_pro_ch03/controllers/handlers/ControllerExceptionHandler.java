2fbfe116337d2c52cd4f0e712279822fb6649d4e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-11 22:06:07 -0300

    methodArgumentNotValid exception handling

--- src/main/java/dev/fernando/spring_pro_ch03/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/dev/fernando/spring_pro_ch03/controllers/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,12 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import dev.fernando.spring_pro_ch03.dto.CustomError;
+import dev.fernando.spring_pro_ch03.dto.ValidationError;
 import dev.fernando.spring_pro_ch03.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -18,5 +20,15 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
         var status = HttpStatus.NOT_FOUND.value();
         return ResponseEntity.status(status).body(new CustomError(Instant.now(), status, e.getMessage(), request.getRequestURI()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        var status = HttpStatus.UNPROCESSABLE_ENTITY.value();
+        var error = new ValidationError(Instant.now(), status, "Dados inválidos!", request.getRequestURI());
+        e.getBindingResult()
+        .getFieldErrors()
+        .forEach(fieldError -> error.addError(fieldError.getField(), fieldError.getDefaultMessage()));
+        return ResponseEntity.status(status).body(error);
     }
 }
---
+++ src/main/java/dev/fernando/spring_pro_ch03/dto/FieldMessage.java
@@ -0,0 +1,4 @@
+package dev.fernando.spring_pro_ch03.dto;
+
+public record FieldMessage(String fieldName, String message) {
+}
